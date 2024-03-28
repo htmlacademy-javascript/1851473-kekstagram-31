f44b1e3f1d1c377d702f3791b371730f0bd60c16
Igor <3-tv-n@mail.ru>
2024-03-29 01:49:35 +0300

    Сделал рефакторинг кода на фильтры попапа

--- js/popup-upload-img/create-slider.js
+++ js/popup-upload-img/create-slider.js
@@ -3,6 +3,56 @@ const sliderInput = document.querySelector('.effect-level__value');
 const filtersRadio = document.querySelectorAll('.effects__radio');
 const imagePreview = document.querySelector('.img-upload__preview img');
 const rangeFilter = document.querySelector('.img-upload__effect-level');
+const chrome = {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  filter: 'grayscale',
+  unit: ''
+};
+const sepia = {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  filter: 'sepia',
+  unit: ''
+};
+const marvin = {
+  range: {
+    min: 0,
+    max: 100
+  },
+  start: 100,
+  step: 1,
+  filter: 'invert',
+  unit: '%'
+};
+const phobos = {
+  range: {
+    min: 0,
+    max: 3
+  },
+  start: 3,
+  step: 0.1,
+  filter: 'blur',
+  unit: 'px'
+};
+const heat = {
+  range: {
+    min: 0,
+    max: 3
+  },
+  start: 3,
+  step: 0.1,
+  filter: 'brightness',
+  unit: ''
+};
 
 
 
@@ -28,8 +78,24 @@ function filterRangeSlider () {
     },
   });
 
+  function reloadValueSlider (filterStyleString) {
+    let filterStyle = eval(filterStyleString);
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: filterStyle.range.min,
+        max: filterStyle.range.max,
+      },
+      start: filterStyle.start,
+      step: filterStyle.step
+    });
+    sliderElement.noUiSlider.on('update', function () {
+      sliderInput.value = sliderElement.noUiSlider.get();
+      imagePreview.style.filter = filterStyle.filter + '(' + sliderInput.value + filterStyle.unit + ')';
+    });
+    rangeFilter.classList.remove('hidden');
+  }
+
   rangeFilter.classList.add('hidden');
-  console.log('ghbdtn');
 
   sliderElement.noUiSlider.on('update', function () {
     sliderInput.value = sliderElement.noUiSlider.get();
@@ -38,80 +104,11 @@ function filterRangeSlider () {
   filtersRadio.forEach( function (filterRadio) {
     filterRadio.addEventListener('change', function (evt) {
       const nameEffect = filterRadio.value;
-
-      if (nameEffect === 'chrome') {
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1,
-          },
-          start: 1,
-          step: 0.1
-        });
-        sliderElement.noUiSlider.on('update', function () {
-          sliderInput.value = sliderElement.noUiSlider.get();
-          imagePreview.style.filter = `grayscale(${sliderInput.value})`;
-        });
-        rangeFilter.classList.remove('hidden');
-      }  else if (nameEffect === 'sepia') {
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1,
-          },
-          start: 1,
-          step: 0.1
-        });
-        sliderElement.noUiSlider.on('update', function () {
-          sliderInput.value = sliderElement.noUiSlider.get();
-          imagePreview.style.filter = `sepia(${sliderInput.value})`;
-        });
-        rangeFilter.classList.remove('hidden');
-      } else if (nameEffect === 'marvin') {
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 100
-          },
-          start: 100,
-          step: 1
-        });
-        sliderElement.noUiSlider.on('update', function () {
-          sliderInput.value = sliderElement.noUiSlider.get();
-          imagePreview.style.filter = `invert(${sliderInput.value}%)`;
-        });
-        rangeFilter.classList.remove('hidden');
-      } else if (nameEffect === 'phobos') {
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 3
-          },
-          start: 3,
-          step: 0.1
-        });
-        sliderElement.noUiSlider.on('update', function () {
-          sliderInput.value = sliderElement.noUiSlider.get();
-          imagePreview.style.filter = `blur(${sliderInput.value}px)`;
-        });
-        rangeFilter.classList.remove('hidden');
-      } else if (nameEffect === 'heat') {
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 3
-          },
-          start: 3,
-          step: 0.1
-        });
-        sliderElement.noUiSlider.on('update', function () {
-          sliderInput.value = sliderElement.noUiSlider.get();
-          imagePreview.style.filter = `brightness(${sliderInput.value})`;
-        });
-        rangeFilter.classList.remove('hidden');
-      } else {
+      if (nameEffect === 'none') {
         imagePreview.style.filter = null;
         rangeFilter.classList.add('hidden');
+      } else {
+        reloadValueSlider(nameEffect);
       }
     })
   });
